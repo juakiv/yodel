95fd1adf9247057ed329d3148f092f3e3d7aaee7
Jussi <juakiv@utu.fi>
2023-06-27 15:37:24 +0300

    kirjautuminen valmis

---
+++ src/app/api/login/route.js
@@ -0,0 +1,65 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+
+import prisma from "@/lib/server/prisma";
+import validateServerSession from "@/lib/server/validateServerSession";
+
+export async function POST(request) {
+  const user = await validateServerSession();
+
+  if(user) {
+    return NextResponse.json({
+      success: false
+    }, {
+      status: 401
+    });
+  }
+
+  const data = await request.json();
+
+  if (!["email", "password"].every(field => Object.keys(data).includes(field))) {
+    return NextResponse.json({
+      success: false
+    }, {
+      status: 422
+    });
+  }
+
+  const findEmail = await prisma.user.findFirst({
+    where: {
+      email: data.email
+    }
+  });
+
+  if (!findEmail) {
+    return NextResponse.json({
+      success: false
+    }, {
+      status: 422
+    });
+  }
+  
+  const passwordMatch = await bcrypt.compare(data.password, findEmail.password);
+  if(!passwordMatch) {
+    return NextResponse.json({
+      success: false
+    }, {
+      status: 422
+    });
+  }
+  
+  const token = [...Array(40)].map(() => Math.random().toString(36)[2]).join('');
+  await prisma.session.create({
+    data: {
+      token: token,
+      expiresAt: new Date(new Date().setMonth(new Date().getMonth() + 1)).toISOString(),
+      userId: findEmail.id
+    }
+  });
+
+  const response = NextResponse.json({
+    success: true
+  });
+  response.cookies.set("sessionToken", token);
+  return response;
+}
--- src/app/login/page.js
+++ src/app/login/page.js
@@ -15,7 +15,43 @@ export default function Login() {
     }
   }, [user]);
 
+  const handleLoginSubmit = async e => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.target);
+    const { email, password } = Object.fromEntries(formData);
+
+    const result = await fetch("/api/login", {
+      method: "POST",
+      body: JSON.stringify({ email, password })
+    });
+    const { success } = await result.json();
+
+    if(success) {
+      router.push("/");
+      router.refresh();
+    } else {
+      alert("hups");
+    }
+  }
+
   return (
-    <>kirjautumissivu</>
+    <div className="auth-page">
+      <img width="100" height="100" src="/logo.png" style={{ marginBottom: 20, borderRadius: 6 }} />
+      <h1>Kirjaudu</h1>
+
+      <form onSubmit={e => handleLoginSubmit(e)}>
+        <label>
+          Sähköpostiosoite
+          <input type="email" name="email" />
+        </label>
+        <label>
+          Salasana
+          <input type="password" name="password" />
+        </label>
+
+        <button type="submit">Kirjaudu sisään</button>
+      </form>
+    </div>
   )
 }
